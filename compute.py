2e260444373ea9d7e2840488008b98f97b4eadd4
Catherine Johnson <cfjohnson@earthlink.net>
2018-09-14 22:59:47 -0600

    found sd and mean

--- compute.py
+++ compute.py
@@ -17,17 +17,47 @@ import math
 
 from error_in_s_o_l import er_in_sol
 
-def read_nums():
+def get_mean(length):
     total = 0;
-    length = len(er_in_sol)
     for element in er_in_sol:
+        total = total + element
+    return total / length
+
+def get_dev_from_mean(mean):
+    set_of_deviations = []
+    for element in er_in_sol:
+        set_of_deviations.insert(0, (mean - element))
+    return set_of_deviations
+
+
+def square_deviations(set_of_devs):
+    set_of_deviations_squared = []
+    for element in set_of_devs:
+        set_of_deviations_squared.insert(0, math.pow(element, 2))
+    return set_of_deviations_squared
+
+
+def sum_squared_deviations(squared_deviations):
+    total = 0;
+    for element in squared_deviations:
         total = total + element;
-    mean = total/length;
-    print('mean: ', mean)
+    return total
 
 
+def calc_inferences_from_data():
+    length = len(er_in_sol)
+    mean = get_mean(length)
+    deviation_from_mean_array = get_dev_from_mean(mean)
+    squared_deviation_from_mean_array = square_deviations(deviation_from_mean_array)
+    sum_of_squared_deviations = sum_squared_deviations(squared_deviation_from_mean_array)
+    approx_avg_of_devs = sum_of_squared_deviations/(length - 1)
+    standard_deviation = math.sqrt(approx_avg_of_devs)
+    standard_error = standard_deviation/math.sqrt(length)
+    print('sd: ', standard_deviation)
+    print('standard_error: ', standard_error)
+    print('length', length)
 
 
-read_nums()
+calc_inferences_from_data()
 
 
