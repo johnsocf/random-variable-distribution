4f55106fc96df87df1d88c18fd5f2275d9369294
Catherine Johnson <cfjohnson@earthlink.net>
2018-09-16 20:26:35 -0600

    update main

--- compute.py
+++ compute.py
@@ -18,17 +18,25 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import interactive
+from scipy.stats import norm
 
 import scipy.stats as stats
 
 from error_in_s_o_l import er_in_sol
+from r_a_d import g_counts, g_time
 
 
-def get_mean(length):
+def get_mean_for_continuous(data, length):
     total = 0;
-    for element in er_in_sol:
+    for element in data:
         total = total + element
     return total / length
+
+def get_mean_for_discrete(data_set_timeline, probability_set, length):
+    mean_sum = 0
+    for indx, element in enumerate(data_set_timeline):
+        mean_sum = mean_sum + (data_set_timeline([indx] * probability_set))
+    return (mean_sum/ length)
 
 
 def get_dev_from_mean(mean):
@@ -58,14 +66,14 @@ def investigate_histogram_in_np(random_vars):
     print('bin edges', bin_edges)
 
 
-def build_histogram_in_matplotlib(random_vars, figure_num, interactive_bool):
+def build_histogram_in_matplotlib(data, title, figure_num, interactive_bool):
     plt.figure(figure_num)
-    n, bins, patches = plt.hist(x=random_vars, bins='auto', color='#0504aa',
+    n, bins, patches = plt.hist(x=data, bins='auto', color='#0504aa',
                                 alpha=0.7, rwidth=0.85)
     plt.grid(axis='y', alpha=.75)
     plt.xlabel('value')
     plt.ylabel('frequency')
-    plt.title('histogram of data')
+    plt.title(title)
     plt.text(23, 45, r'$\mu=15, b=3$')
     maxfreq = n.max()
     plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
@@ -83,7 +91,7 @@ def build_q_q_plot_using_stats(data, title, figure_num, interactive_bool):
 
 def calc_inferences_from_data(data):
     length = len(data)
-    mean = get_mean(length)
+    mean = get_mean_for_continuous(data, length)
     deviation_from_mean_array = get_dev_from_mean(mean)
     squared_deviation_from_mean_array = square_deviations(deviation_from_mean_array)
     sum_of_squared_deviations = sum_squared_deviations(squared_deviation_from_mean_array)
@@ -93,16 +101,59 @@ def calc_inferences_from_data(data):
     return mean, standard_deviation
 
 
+def plot_pdf(data_set):
+    # ???
+    fig, ax = plt.subplots(1, 1)
+    x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
+    ax.plot(data_set, norm.pdf(x), 'r-', lw=5, alpha=0.6, label='norm pdf')
+    rv = norm()
+    ax.plot(data_set, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+    vals = norm.ppf([0.001, 0.5, 0.999])
+    np.allclose([0.001, 0.5, 0.999], norm.cdf(vals))
+    r = norm.rvs(size=1000)
+    ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
+    ax.legend(loc='best', frameon=False)
+    interactive(False)
+    plt.show()
+
 def build_plots_for_speed_of_light(initial_data_set, random_var_built_data_set):
     build_q_q_plot_using_stats(initial_data_set, 'q q plot of original data', 1, True)
-    build_histogram_in_matplotlib(random_var_built_data_set, 2, True)
+
+    plot_pdf(random_var_built_data_set)
+    build_histogram_in_matplotlib(random_var_built_data_set, 'histogram for random variable set speed of light', 2, True)
     build_q_q_plot_using_stats(random_var_built_data_set, 'q q plot of random var', 3, False)
 
-
-def init():
+def set_up_speed_of_light_dist():
     mean, standard_d = calc_inferences_from_data(er_in_sol)
     random_var_set_speed_of_light = [random.gauss(mean, standard_d) for _ in range(100)]
     build_plots_for_speed_of_light(er_in_sol, random_var_set_speed_of_light)
+
+def set_up_geiger_dist():
+    # X = # geiser counts over time.
+    length = len(g_counts)
+
+    # Construct a random variable that models the geiger counts **
+
+    # get probability dist for geiser...
+    # stub
+    probability = [0]
+
+
+    # get mean and standard d
+    mean = get_mean_for_discrete(g_time, probability, length)
+
+    # build data set based using rand based on mean and sd?
+
+    # build pdf:
+    build_histogram_in_matplotlib(g_counts, 'histogram for geiger count', 4, True)
+
+    # build pdf
+    # build q q plot (similar to existing function) + (overlay on histogram)
+
+
+def init():
+    set_up_speed_of_light_dist()
+    #set_up_geiger_dist()
 
 
 init()
